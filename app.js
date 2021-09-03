13832697283d9a30c9326e5d3bfdefa7e63571f0
rajeshprofile <hirajesh.official@gmail.com>
2021-09-03 20:38:50 +0530

    backend the-siren

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require('dotenv').config();
-const cors = require("cors");
+const cors = require('cors');
 // Route imports
 const homeRoutes = require("./routes/home");
 const bollywoodRoutes = require("./routes/bollywood");
